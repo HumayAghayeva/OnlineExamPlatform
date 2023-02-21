97ef76d76b908a714633527bf298e31e2cd5a17b
humayra <humayra@code.edu.az>
2023-02-21 12:59:16 +0400

    Remove unnecessary projects

--- RegistrationWebApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace OnlineExamPlatFormWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
